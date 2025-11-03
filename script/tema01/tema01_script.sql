9eaaa8d45fd4283feebfe6a4495a5fe8a119b340
vanesaarandavera <vanesaa715@gmail.com>
2025-11-02 23:03:07 -0300

    se agrego un ejemplo de función   historial de paciente

--- script/tema01/tema01_script.sql
+++ script/tema01/tema01_script.sql
@@ -16,3 +16,36 @@ END
 GO;
 --para cargar atencion
  EXEC SP_RegistrarAtencion 1, 5, 2, 8;
+ 
+
+ -- Función para obtener el historial de atenciones de un paciente
+CREATE FUNCTION FN_HistorialAtencionesPaciente (@p_idPaciente INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT
+        a.fechaHora,
+        p.nombreCompleto AS Profesional,
+        e.nombre AS Especialidad,
+        d.descripcion AS Diagnostico
+    FROM
+        Atencion a
+    JOIN
+        Profesional p ON a.idProfesional = p.idProfesional
+    JOIN
+        Especialidad e ON p.idEspecialidad = e.idEspecialidad [cite: 6]
+    JOIN
+        Diagnostico d ON a.idDiagnostico = d.idDiagnostico
+    WHERE
+        a.idPaciente = @p_idPaciente
+);
+GO*/
+
+--uso consulta historial de atencion de un paciente
+SELECT
+    *
+FROM
+    dbo.FN_HistorialAtencionesPaciente(5) -- Muestra el historial del paciente con idPaciente = 5
+ORDER BY
+    fechaHora DESC;
