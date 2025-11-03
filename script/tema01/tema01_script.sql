67e3e9c7c06943a817b399c123961ada33e74f45
vanesaarandavera <vanesaa715@gmail.com>
2025-11-02 23:12:05 -0300

    cambiojde ejemplo de procedimiento

--- script/tema01/tema01_script.sql
+++ script/tema01/tema01_script.sql
@@ -1,22 +1,30 @@
--- Ejemplo 1: Procedimiento para registrar una nueva atención
-CREATE PROCEDURE SP_RegistrarAtencion
-    @p_idUbicacionMovil INT,
-    @p_idPaciente INT,
-    @p_idProfesional INT,
-    @p_idDiagnostico INT
+ --Procedimiento para obtener unidades móviles por barrio (actuales)
+CREATE PROCEDURE SP_ObtenerUnidadesPorBarrioActual
+    @p_idBarrio INT
 AS
 BEGIN
-    -- Validaciones o lógica de negocio aquí (ej: verificar capacidad, horarios, etc.)
+    SELECT
+        um.patente,
+        tum.descripcion AS TipoUnidad,
+        um.capacidadDiaria,
+        b.nombre_barrio,
+        uc.direccion,
+        uc.fecha_ingreso
+    FROM
+        UnidadMovil um
+    JOIN
+        TipoUnidadMovil tum ON um.idTipo = tum.idTipo
+    JOIN
+        UbicacionMovil uc ON um.idUnidad = uc.idUnidad
+    JOIN
+        Barrio b ON uc.idBarrio = b.idBarrio
+    WHERE
+        uc.idBarrio = @p_idBarrio
+        AND uc.fecha_egreso IS NULL; -- Solo ubicaciones actuales 
+END;
 
-    INSERT INTO Atencion (idUbicacionMovil, idPaciente, idProfesional, idDiagnostico)
-    VALUES (@p_idUbicacionMovil, @p_idPaciente, @p_idProfesional, @p_idDiagnostico);
 
-    SELECT SCOPE_IDENTITY() AS NuevoIdAtencion; -- Devuelve el ID de la nueva atención
-END
-GO;
---para cargar atencion
- EXEC SP_RegistrarAtencion 1, 5, 2, 8;
- 
+ EXEC SP_ObtenerUnidadesPorBarrioActual 10;
 
  -- Función para obtener el historial de atenciones de un paciente
 CREATE FUNCTION FN_HistorialAtencionesPaciente (@p_idPaciente INT)
