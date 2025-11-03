104894bcaf0864235b9398cb8d16ed8ee8863a60
Sebastian Prado <sebianp@gmail.com>
2025-11-03 10:28:43 -0300

    Update: Transaccion Implicita para agregar atención exitosa y fallida.

--- script/tema3_Manejo_de_transacciones/transacciones_script.sql
+++ script/tema3_Manejo_de_transacciones/transacciones_script.sql
@@ -1,11 +1,11 @@
 
 --Objetivo: 
 --registrar una atencion medica junto con los síntomas del paciente, y asegurarte de que no se registre nada si falla alguna parte.
-
+-- Transaccion para Atencion, Sintomas y tratamientos
 BEGIN TRANSACTION;
 BEGIN TRY
     -- Insertar atención
-    INSERT INTO Atencion (fechaHora, idUnidad, idPaciente, idProfesional, idDiagnostico)
+    INSERT INTO Atencion (fechaHora, idUbicacionMovil, idPaciente, idProfesional, idDiagnostico)
     VALUES (GETDATE(), 5, 7, 5, 3);
 
     DECLARE @idAtencion INT = SCOPE_IDENTITY();
@@ -26,11 +26,32 @@ END CATCH;
 go
 
 --Provocar un error: Se modifica un sintoma y se coloca un idSintoma no registrado.
-INSERT INTO Atencion_Sintoma (idAtencion, idSintoma) VALUES (@idAtencion, 29); --No existe el sintoma 29, solo hay hasta 10
+BEGIN TRANSACTION;
+BEGIN TRY
+    -- Insertar atención
+    INSERT INTO Atencion (fechaHora, idUbicacionMovil, idPaciente, idProfesional, idDiagnostico)
+    VALUES (GETDATE(), 7, 2, 5, 3);
+
+    DECLARE @idAtencion INT = SCOPE_IDENTITY();
+
+    -- Insertar síntomas asociados (provocar error poniendo un sintoma que no existe)
+    INSERT INTO Atencion_Sintoma (idAtencion, idSintoma) VALUES (@idAtencion, 3);
+    INSERT INTO Atencion_Sintoma (idAtencion, idSintoma) VALUES (@idAtencion, 17); --Error Provocado (No existe sintoma 17)
+	INSERT INTO Atencion_Tratamiento (idAtencion, idTratamiento) VALUES (@idAtencion, 3);
+    INSERT INTO Atencion_Tratamiento (idAtencion, idTratamiento) VALUES (@idAtencion, 1);
+
+    COMMIT TRANSACTION;
+    PRINT 'Atención y síntomas registrados correctamente.';
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+    PRINT 'Error detectado. Se canceló la transacción.';
+END CATCH;
+go
 
 --Comprobacion viendo por fecha se puede ver si se agrego o no, dependiendo si se provoco un fallo
-select a.idAtencion, a.idPaciente, a.idProfesional , a.idUnidad, a.fechaHora, s.idSintoma
-from Atencion as a inner join Atencion_Sintoma as s on a.idAtencion = s.idAtencion
+select a.idAtencion, a.idPaciente, a.idProfesional , a.idUnidad, a.fechaHora
+from Atencion as a
 order by a.fechaHora desc
 
 --Controles
